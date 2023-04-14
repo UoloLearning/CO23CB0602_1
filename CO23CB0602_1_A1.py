6759beb4903f621aa4ed67a4d3f388dce60be2c9
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-14 17:40:56 +0530

    Create CO23CB0602_1_A1.py

---
+++ CO23CB0602_1_A1.py
@@ -0,0 +1,10 @@
+sentence1 = "John loves to play football. "
+sentence2 = "They have a match with another team next week. "
+
+sentence3 = "He plays football with his friends every weekend. "
+sentence4 = "They are well-prepared for the match. "
+
+print(sentence1)
+print(sentence3)
+print(sentence2)
+print(sentence4)
